293e658bca525c9e572d774b3eb596f6323597a0
Shashi Raj Singh <singh.shashiraj@gmail.com>
2019-06-10 23:59:15 +0800

    lint away

--- src/graphql/service.js
+++ src/graphql/service.js
@@ -1,23 +1,19 @@
 
 const { ApolloServer } = require('apollo-server-express')
 
-const graphqlCompse = (typeDefs, resolvers, repo) => {
-
- return new ApolloServer({
-   typeDefs,
-   resolvers,
-   context: ({ req }) => ({
-     req,
-     repo
-    }),
-    playground: {
-      endpoint: 'http://localhost:3000/graphql',
-      settings: {
-        'editor.theme': 'light',
-      },
+const graphqlCompse = (typeDefs, resolvers, repo) => new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({
+    req,
+    repo,
+  }),
+  playground: {
+    endpoint: 'http://localhost:3000/graphql',
+    settings: {
+      'editor.theme': 'light',
     },
-  });
+  },
+})
 
-}
-
-module.exports = Object.assign({}, { graphqlCompse } )
+module.exports = Object.assign({}, { graphqlCompse })
