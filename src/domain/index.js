17f5b7571ea237be12764385dee6464dee1d1e39
Shashi Raj Singh <singh.shashiraj@gmail.com>
2019-06-10 13:12:30 +0800

    appcserver

--- src/domain/index.js
+++ src/domain/index.js
@@ -4,5 +4,4 @@ const { User, Incident } = require('./models')
 const handlers = require('./repository/handlers')
 
 const dbService = handlers(Incident, User)
-
 module.exports = dbService
--- src/server.js
+++ src/server.js
@@ -1,37 +1,27 @@
-const config = require('config')
-const express = require('express')
-const mongoose = require('mongoose')
-const { ApolloServer } = require('apollo-server-express')
+'use strict'
 
-mongoose.Promise = global.Promise
+const express = require(`express`)
+const graphQLSERVER = require('./graphql/schema.js')
+const config = require('config')
+const mongoose  = require('mongoose')
+mongoose.Promise  = global.Promise
 
 const { seedUsers } = require('./db-init')
 
 mongoose.connect(config.get('db.uri'), { useNewUrlParser: true })
   .then(async () => {
-
     console.log('INFO: Connected to the database')
 
     await seedUsers()
-
-    // TODO: Initialize Apollo with the required arguments as you see fit
-    const server = new ApolloServer({})
-
     const app = express()
-    server.applyMiddleware({ app })
-
+    graphQLSERVER.applyMiddleware({ app })
     const { host, port } = config.get('server')
 
     app.listen({ port }, () => {
-
-      console.log(`Server ready at http://${host}:${port}${server.graphqlPath}`)
-
+      console.log(`Server ready at http://${ host }:${ port }${ graphQLSERVER.graphqlPath }`)
     })
-
   })
   .catch((error) => {
-
     console.error(error)
     process.exit(-1)
-
   })
